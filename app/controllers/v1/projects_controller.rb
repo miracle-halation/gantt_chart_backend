df3aa641b3302bd580e2ec88964d6ed295bdd7f4
miracle-halation <miraclehalation.9710@gmail.com>
2022-08-01 21:52:48 +0900

    projectコントローラー作成

---
+++ app/controllers/v1/projects_controller.rb
@@ -0,0 +1,2 @@
+class V1::ProjectsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :v1 do
     mount_devise_token_auth_for 'User', at: 'auth'
     resources :groups, only: [:index]
+    resources :projects, only: [:index, :show, :create, :edit, :update, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
