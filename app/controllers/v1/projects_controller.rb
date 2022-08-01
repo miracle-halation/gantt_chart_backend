cedd921cb6970ba75d1ed2094ed4a767d65be376
miracle-halation <miraclehalation.9710@gmail.com>
2022-08-01 23:01:44 +0900

    projectにデータを作成することができる

--- app/controllers/v1/projects_controller.rb
+++ app/controllers/v1/projects_controller.rb
@@ -1,2 +1,20 @@
 class V1::ProjectsController < ApplicationController
+	def index
+		projects = Project.all()
+		render json: projects
+	end
+
+	def create
+		project = Project.new(project_params)
+		if project.save
+			render json: project
+		else
+			render json: ['error': '作成に失敗しました', 'errors_msg': project.errors.full_messages]
+		end
+	end
+
+	private
+	def project_params
+		params.require(:project).permit(:title, :category, :url, :deadline, :group_id)
+	end
 end
