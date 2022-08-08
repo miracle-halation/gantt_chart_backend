44d8408aacf87f0aa52814a8eeaa54d6680af1ec
miracle-halation <miraclehalation.9710@gmail.com>
2022-08-08 21:47:32 +0900

    profileコントローラー作成

---
+++ app/controllers/v1/profiles_controller.rb
@@ -0,0 +1,2 @@
+class V1::ProfilesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :v1 do
     mount_devise_token_auth_for 'User', at: 'auth'
+    resources :profiles, only: [:index, :show, :create, :edit, :update, :destroy]
     resources :projects, only: [:index, :show, :create, :edit, :update, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
