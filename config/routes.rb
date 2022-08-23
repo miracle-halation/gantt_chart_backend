e59159f4f4257d42fc992430111c07a5c67a13cc
miracle-halation <miraclehalation.9710@gmail.com>
2022-08-23 21:36:39 +0900

    ルーティングを一部変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :v1 do
     mount_devise_token_auth_for 'User', at: 'auth'
-    resources :profiles, only: [:show, :create, :edit, :update]
+    resource :profiles, only: [:show, :create, :edit, :update]
     resources :projects, only: [:index, :show, :create, :edit, :update, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
