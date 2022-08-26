dccaa2aeb61f024996f1ba14b4b1d5ce021f1b92
miracle-halation <miraclehalation.9710@gmail.com>
2022-08-26 22:33:51 +0900

    rubocopでコード整形

--- spec/requests/v1/profiles_spec.rb
+++ spec/requests/v1/profiles_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe 'V1::Profiles', type: :request do
     context '成功する時' do
       it '値が正しい時に作成に成功する' do
         post_params = { profile: { name: 'test_create', phone: '000-1111-1111', group: 'システム部' } }
-        expect {post v1_profiles_path, params: post_params, headers: auth_headers}.to change(Profile, :count).by(1)
+        expect { post v1_profiles_path, params: post_params, headers: auth_headers }.to change(Profile, :count).by(1)
         expect(response.status).to eq(200)
         json = JSON.parse(response.body)
         expect(json['data']['name']).to eq post_params[:profile][:name]
@@ -28,7 +28,7 @@ RSpec.describe 'V1::Profiles', type: :request do
     context '失敗する時' do
       it '値が間違っている時に作成に失敗してエラーを返す' do
         post_params = { profile: { name: '', phone: '000-1111-1111', group: '' } }
-        expect {post v1_profiles_path, params: post_params, headers: auth_headers}.to change(Profile, :count).by(0)
+        expect { post v1_profiles_path, params: post_params, headers: auth_headers }.to change(Profile, :count).by(0)
         expect(response.status).to eq(200)
         json = JSON.parse(response.body)
         expect(json[0]['error']).to eq '作成に失敗しました'
@@ -37,10 +37,10 @@ RSpec.describe 'V1::Profiles', type: :request do
       end
     end
   end
-  describe "Put /v1/profiles" do
+  describe 'Put /v1/profiles' do
     let!(:profile) { FactoryBot.create(:profile, name: 'test_old', user_id: user.id) }
-    context "成功する時" do
-      it "値が正しい時にprofileを更新することができる" do
+    context '成功する時' do
+      it '値が正しい時にprofileを更新することができる' do
         post_params = { profile: { name: 'test_update', phone: '000-1111-0000', group: '企画部' } }
         put v1_profiles_path, params: post_params, headers: auth_headers
         expect(response.status).to eq(200)
@@ -49,8 +49,8 @@ RSpec.describe 'V1::Profiles', type: :request do
         expect(json['data']['name']).to eq post_params[:profile][:name]
       end
     end
-    context "失敗する時" do
-      it "値が間違っている時にprofileの更新に失敗してエラーメッセージを返す" do
+    context '失敗する時' do
+      it '値が間違っている時にprofileの更新に失敗してエラーメッセージを返す' do
         post_params = { profile: { name: '', phone: '000-1111-0000', group: '' } }
         put v1_profiles_path, params: post_params, headers: auth_headers
         expect(response.status).to eq(200)
